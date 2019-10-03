524b03b4e2e701eedb48c8c4e54363d68e61c5f3
Khadijah Grimes <khadijahgrimes7@gmail.com>
2019-10-02 23:10:52 -0500

    updated again 1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,22 @@
 // generate a random number at start 
 
+// random number for start of game should  be from 19-120
+// r=crystal have hidden value from 1-12. 
 
-*// var randNumb = Math.floor(Math.random() * 300) + 100;
+*// var randNumb = Math.floor(Math.random() * 120) + 19;
     //console.log(randNumb)
+    // functions are mini programs mini snips of 
+    //  code within the code, a function will only run once
+    //  it's called. 
 
+    // declaring the click counter put it at the top of 
+    the script or outside of it // 
 
-    // on.click function for button - jquery - 
-    what is it going
-to do once its clicked ?
+// on.click function for buttons(gems)- jquery - 
+click count needs to fire off the
+
+    ** what is it going
+to do once its clicked ?**
 
     everytime gem is clicked{
     make something that stores the click count
@@ -18,6 +27,40 @@ everytime its clicked add to the value {
     I change it inside the Counter
 }
 
-global {
-    something n
+jquery how to hide elements  {
+    hide the amount until player clicks a gem.
 }
+
+when they do click the gem{
+    update the players score counter
+}
+
+Player wins if their total score matches random number{
+    from the beginning of game, make a trigger to let
+        game know once it has reached that # that they won
+}
+
+else player loses if their score goes above {
+    the random number
+}
+
+
+
+reset function {
+    if and when player wins or loses
+}
+
+Game begins again player sees new random # {
+    all the crystals will then have 4 new hidden
+    values
+
+}
+
+users score  and score counter{
+    will reset to zero. 
+}
+
+let app show # of games the player w/l {
+do not refresh page to restart game.
+}
+
