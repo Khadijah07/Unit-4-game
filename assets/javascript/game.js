ab4f6d96c1532678a62120944a566524b63771d5
Khadijah Grimes <khadijahgrimes7@gmail.com>
2019-10-02 20:35:26 -0500

    updated again

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+// generate a random number at start 
+
+
+*// var randNumb = Math.floor(Math.random() * 300) + 100;
+    //console.log(randNumb)
+
+
+    // on.click function for button - jquery - 
+    what is it going
+to do once its clicked ?
+
+    everytime gem is clicked{
+    make something that stores the click count
+}
+
+everytime its clicked add to the value {
+    where would you declare this and how would
+    I change it inside the Counter
+}
+
+global {
+    something n
+}
